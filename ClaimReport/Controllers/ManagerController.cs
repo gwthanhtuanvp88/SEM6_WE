ed7a7a09a611ea3b560d19b8a62072a47d673343
Sang <sangnh89@gmail.com>
2017-04-07 06:16:21 +0700

    report

--- ClaimReport/Controllers/ManagerController.cs
+++ ClaimReport/Controllers/ManagerController.cs
@@ -118,5 +118,126 @@ namespace ClaimReport.Controllers
                 datas = datas
             });
         }
+
+        [HttpPost]
+        public JsonResult Number_of_students_making_a_claim_within_each_Faculty_for_each_academic_year()
+        {
+            List<Academyyear> acadmemy = db.Academyyears.ToList();
+            List<string> labels = new List<string>();
+            List<string> label = new List<string>();
+            List<List<int?>> datas = new List<List<int?>>();
+            var i = 0;
+            foreach (var item in acadmemy)
+            {
+                labels.Add(item.name);
+                var result = db.Number_of_students_making_a_claim_within_each_Faculty_for_each_academic_year(item.id).ToList();
+                List<int?> data = new List<int?>();
+                foreach (var item1 in result)
+                {
+                    if (i == 0)
+                    {
+                        label.Add(item1.name);
+                    }
+                    data.Add(item1.student);
+                }
+                datas.Add(data);
+                i++;
+            }
+
+            return Json(new
+            {
+                labels = labels,
+                label = label,
+                datas = datas
+            });
+        }
+
+        public ActionResult AcademyYearReport()
+        {
+            ViewBag.AcademyYear = db.Academyyears.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Percentage_of_claims_by_each_Faculty_for_any_cademic_year(int academyYearID)
+        {
+            var result = db.Number_of_claims_within_each_Faculty_for_each_academic_year(academyYearID).ToList();
+
+            List<Academyyear> acadmemy = db.Academyyears.ToList();
+            List<string> labels = new List<string>();
+            List<int?> data = new List<int?>();
+
+            foreach (var item in result)
+            {
+                labels.Add(item.name);
+                data.Add(item.claims);
+            }
+
+            return Json(new
+            {
+                labels = labels,
+                data = data
+            });
+        }
+
+        [HttpPost]
+        public JsonResult Claims_without_uploaded_evidence(int academyYearID)
+        {
+            var result = db.Claims_without_uploaded_evidence(academyYearID).ToList();
+            var total = db.Claims.ToList().Count;
+            List<string> labels = new List<string>();
+            labels.Add("Evidences");
+            labels.Add("No Evidence");
+
+            List<int> data = new List<int>();
+            data.Add(total - result[0].Value);
+            data.Add(result[0].Value);
+            
+            return Json(new
+            {
+                labels = labels,
+                data = data
+            });
+        }
+
+        [HttpPost]
+        public JsonResult Claims_without_a_decision_after_14_days(int academyYearID)
+        {
+            var result = db.Claims_without_a_decision_after_14_days(academyYearID).ToList();
+            var total = db.Claims.ToList().Count;
+            List<string> labels = new List<string>();
+            labels.Add("Processed");
+            labels.Add("no process");
+
+            List<int> data = new List<int>();
+            data.Add(total - result[0].Value);
+            data.Add(result[0].Value);
+
+            return Json(new
+            {
+                labels = labels,
+                data = data
+            });
+        }
+
+        [HttpPost]
+        public JsonResult day_has_the_most_claim(int academyYearID)
+        {
+            var result = db.day_has_the_most_claim(academyYearID).ToList();
+            List<string> labels = new List<string>();
+            List<int?> data = new List<int?>();
+
+            foreach (var item in result)
+            {
+                labels.Add(Convert.ToDateTime(item.datesubmited).ToString("MM/dd//yyy"));
+                data.Add(item.claims);
+            }
+
+            return Json(new
+            {
+                labels = labels,
+                data = data
+            });
+        }
     }
 }
